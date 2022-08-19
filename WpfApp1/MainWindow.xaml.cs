8fdf524c428e6e021b4d3dd1afa5e3151adcc8ac
AWSMProdigy <StraightAKyle@gmail.com>
2022-08-18 22:21:29 -0400

    Can now find single summoner in champ select

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,9 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Http;
 using System.Diagnostics;
 using LCUSharp;
 using LCUSharp.Websocket;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Tab;
 
 namespace WpfApp1
 {
@@ -25,6 +27,7 @@ namespace WpfApp1
     {
         public event EventHandler<LeagueEvent> GameFlowChanged;
         private readonly TaskCompletionSource<bool> _work = new TaskCompletionSource<bool>(false);
+
         public MainWindow()
         {
             InitializeComponent();
@@ -40,10 +43,50 @@ namespace WpfApp1
             myText.Text = "Connected!";
             GameFlowChanged += OnGameFlowChanged;
             api.EventHandler.Subscribe("/lol-gameflow/v1/gameflow-phase", GameFlowChanged);
-
-            await _work.Task;
             Console.WriteLine("Done.");
 
+            void OnGameFlowChanged(object sender, LeagueEvent e)
+            {
+                var result = e.Data.ToString();
+                var state = string.Empty;
+
+                switch (result)
+                {
+                    case "None":
+                        state = "main menu"; break;
+                    case "Lobby":
+                        state = "lobby";
+                        break;
+                    case "ChampSelect":
+                        state = "champ select";
+                        getSummoners(api);
+                        break;
+                    case "GameStart":
+                        state = "game started";
+                        break;
+                    case "InProgress":
+                        state = "game";
+                        break;
+                    case "WaitingForStats":
+                        state = "waiting for stats";
+                        break;
+                    default:
+                        state = $"unknown state: {result}";
+                        break;
+                }
+
+                // Print new state and set work to complete.
+                Trace.WriteLine(state);
+            }
+
+        }
+
+        public async Task getSummoners(LeagueClientApi api)
+        {
+            await Task.Delay(5000);
+            IEnumerable<string> queryParameters = new string[] {"0"};
+            var json = await api.RequestHandler.GetJsonResponseAsync(HttpMethod.Get, "/lol-champ-select/v1/summoners/0");
+            Trace.WriteLine(json.ToString());
         }
 
         private void OnGameFlowChanged(object sender, LeagueEvent e)
@@ -54,8 +97,7 @@ namespace WpfApp1
             switch (result)
             {
                 case "None":
-                    state = "main menu";
-                    break;
+                    state = "main menu";    break;
                 case "Lobby":
                     state = "lobby";
                     break;
